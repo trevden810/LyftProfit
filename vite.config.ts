edfed0153d453dd9a77f1c6b480fb8a84acf41b3
trevden810 <trevor.bates303@gmail.com>
2025-04-18 16:13:37 -0600

    Manual deployment to gh-pages

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/LyftProfit/', // Set the base path to match your GitHub repository name
-})
